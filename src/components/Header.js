d2417752dd8fd552bf138f966290fa3325e69430
Sheng Jun <sjdude@hotmail.com>
2021-01-20 09:17:43 +0800

    Good Morning!

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,6 +7,7 @@ function Header() {
              
                 
             <h1>NUS BIGGGG Money Banking Admin App</h1>
+            <h2>Good Morning Uli, Gilbert Reshma & Class!</h2>
             <p>By <a href="https://github.com/sjdude/devops-CICD-project">sjdude</a> & NoRicePls</p>
 
         </header>
