b549b3e93a12b552bd720951c0b139a2958d507e
Sheng Jun <sjdude@hotmail.com>
2021-01-20 10:35:19 +0800

    Changed texts

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import logo from './logo.jpg'
 
 function Header() {
     return(
@@ -7,7 +6,7 @@ function Header() {
              
                 
             <h1>NUS BIGGGG Money Banking Admin App</h1>
-            <h2>Good Morning Uli, Gilbert Reshma & Class!</h2>
+            <h2>Good Morning Uli, Gilbert, Reshma & Class! Is it 11am already?</h2>
             <p>By <a href="https://github.com/sjdude/devops-CICD-project">sjdude</a> & NoRicePls</p>
 
         </header>
